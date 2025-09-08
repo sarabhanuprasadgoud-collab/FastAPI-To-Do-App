4ac8b7483d85634412f765a3f456013dc48aca7f
Sara Bhanu Prasad Goud <sarabhanuprasadgoud@gmail.com>
2025-09-08 22:55:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, APIRouter
+from configrations import collection
+from database.schemas import all_tasks
+from database.models import Todo
 
 app = FastAPI()
 router = APIRouter()
 
 @router.get("/")
-asyc def get_all_todos():
+async def get_all_todos():
   data = collection.find()
+  return all_tasks(data)
+
+@router.post("/")
+async def create_task(new_task: Todo):
+  
 @app.include_router(router)
